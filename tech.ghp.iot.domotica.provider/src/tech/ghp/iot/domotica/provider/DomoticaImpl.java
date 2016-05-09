15aa408840278d34df5f7230a7dfbdfc5bcad95f
ghpopovici <ghpopovici@gmail.com>
2016-05-09 10:48:17 +0200

    inlucde code related to Raspbery Pi

--- tech.ghp.iot.domotica.provider/src/tech/ghp/iot/domotica/provider/DomoticaImpl.java
+++ tech.ghp.iot.domotica.provider/src/tech/ghp/iot/domotica/provider/DomoticaImpl.java
@@ -1,21 +1,21 @@
 package tech.ghp.iot.domotica.provider;
 
-//import java.io.Closeable;
-//import java.io.IOException;
+import java.io.Closeable;
+import java.io.IOException;
 
-//import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-//import org.osgi.service.component.annotations.Deactivate;
-//import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
 
-//import com.pi4j.io.gpio.GpioController;
-//import com.pi4j.io.gpio.GpioFactory;
-//import com.pi4j.io.gpio.GpioPinDigitalOutput;
-//import com.pi4j.io.gpio.PinState;
-//import com.pi4j.io.gpio.RaspiPin;
-//import com.pi4j.system.SystemInfo;
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.system.SystemInfo;
 
-//import osgi.enroute.scheduler.api.Scheduler;
+import osgi.enroute.scheduler.api.Scheduler;
 
 import tech.ghp.iot.domotica.api.Domotica;
 
@@ -28,7 +28,7 @@ public class DomoticaImpl implements Domotica{
 	public String upper(String input){
 		return input.toUpperCase();
 	}
-	/*
+	
 	private Scheduler scheduler;
 	private Closeable schedule;
 	
@@ -62,5 +62,5 @@ public class DomoticaImpl implements Domotica{
 	void setScheduler(Scheduler scheduler) {
 		this.scheduler = scheduler;
 	}
-	*/
+	
 }
